eac3d1bc84b254fee116781f1f8172b80ab106b1
faithjua <142774376+faithjua@users.noreply.github.com>
2025-07-17 23:26:15 +0900

    login.controller.js 재수정

--- src/modules/login/login.controller.js
+++ src/modules/login/login.controller.js
@@ -1,19 +1,10 @@
 import { UnauthorizedError } from '../../middlewares/error.js'
 
 export const loginSuccess = (req, res) => {
-    const user = req.user
-
-    const userIdString = user.id ? user.id.toString() : null;
-    const serviceIdString = user.service_id ? user.service_id.toString() : null;
-
+    const { id, email, name } = req.user
     res.success({
         message: '로그인 성공',
-        result: { 
-            user_id: userIdString,
-            service_id: serviceIdString,
-            email: user.email,
-            name: user.name,
-         },
+        result: { service_id: id, email, name },
     })
 }
 
