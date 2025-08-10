9ed7143eab4116ca9b87972cfcb466f030aa6c8d
miinsoo <ms1226f@gmail.com>
2025-08-11 00:36:33 +0900

    cors 설정 추가

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -6,7 +6,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://myfit-test.netlify.app"],
         methods: ["GET", "POST"],
         credentials: true,
     },
