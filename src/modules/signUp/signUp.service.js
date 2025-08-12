1774d031cb5c9b62048ceea109eebfaaff66438a
miinsoo <ms1226f@gmail.com>
2025-08-12 19:50:49 +0900

    기본 프로필 핫 픽스

--- src/modules/signUp/signUp.service.js
+++ src/modules/signUp/signUp.service.js
@@ -99,7 +99,7 @@ const usersService = {
                 high_sector,
                 low_sector,
                 recruiting_status,
-                profile_img: 'https://myfit-bucket-mhfd.s3.ap-northeast-2.amazonaws.com/userProfile/base_profile3.svg',
+                profile_img: 'https://myfit-bucket-mhfd.s3.ap-northeast-2.amazonaws.com/userProfile/base_prifile4.svg',
             },
         })
         console.log('Service 생성 완료')
@@ -214,7 +214,7 @@ const usersService = {
                 high_sector: '',
                 low_sector: '',
                 recruiting_status,
-                profile_img: 'https://myfit-bucket-mhfd.s3.ap-northeast-2.amazonaws.com/userProfile/base_profile3.svg',
+                profile_img: 'https://myfit-bucket-mhfd.s3.ap-northeast-2.amazonaws.com/userProfile/base_prifile4.svg',
             }
         })
 
